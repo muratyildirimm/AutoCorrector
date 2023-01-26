91320c62b27932634936569f9bcc9cd24f254080
muratyildirimm <muratyildirimm495@gmail.com>
2023-01-27 00:22:07 +0300

    word processing feature
    An Auto Correct Project

---
+++ autocorrector/autocorrector/src/test/java/MainTest.java
@@ -0,0 +1,15 @@
+
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+        @Test
+        void main() throws IOException {
+            
+        }
+}
